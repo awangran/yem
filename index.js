fb81e7e7638b53a4289c8c3dd80fa79800ad6455
awangran <ashleero.yin@gmail.com>
2023-01-21 11:34:35 -0500

    light and dark mode on pc size

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function dark() {
+    var  deff= document.querySelector(':root');
+    deff.style.setProperty('--color-default', '#F3E9D2');
+
+    var highlight = document.querySelector(':root');
+    highlight.style.setProperty('--color-highlight', '#272520');
+
+    var lightIcon = document.getElementById("lightmode");
+    var darkIcon = document.getElementById("darkmode");
+
+   lightIcon.style.setProperty('visibility', 'visible');
+    darkIcon.style.setProperty('visibility', 'hidden');
+};
+
+function light() {
+    var  deff= document.querySelector(':root');
+    deff.style.setProperty('--color-default', '#114B5F');
+
+    var highlight = document.querySelector(':root');
+    highlight.style.setProperty('--color-highlight', '#F3E9D2');
+
+    var lightIcon = document.getElementById("lightmode");
+    var darkIcon = document.getElementById("darkmode");
+
+    lightIcon.style.setProperty('visibility', 'hidden')
+    darkIcon.style.setProperty('visibility', 'visible')
+};
